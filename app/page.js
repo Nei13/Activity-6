4a30030e3b66a9d2f47ed31699dc32d689bb6e71
Nei13 <occ.felisilda.niel@gmail.com>
2023-10-11 11:04:42 -0700

    first commit

---
+++ app/page.js
@@ -0,0 +1,27 @@
+import Image from 'next/image'
+import styles from './page.module.css'
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <div className={styles.description}>
+        
+      </div>
+
+      <div className={styles.grid}>
+        <a
+          
+        >
+          <h2>
+            Copy Paste Me : (http://localhost:3000/page)  <span>-&gt;</span>
+          </h2>
+          
+        </a>
+      </div>
+
+
+
+
+    </main>
+  )
+}
